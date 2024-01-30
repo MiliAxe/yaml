b6a94d6f8ba993d1acb173d3b71e3eb7a0559e0b
MiliAxe <miliaxe0@gmail.com>
2024-01-30 19:28:14 +0330

    Added the VAO class

---
+++ include/vao.hpp
@@ -0,0 +1,25 @@
+/**
+ *
+ * @file vao.hpp
+ *
+ * @brief Vertex Array Object (VAO) class
+ *
+ */
+
+#pragma once
+#include <glad/glad.h>
+#include <globals.hpp>
+
+class VAO
+{
+private:
+    GLuint id_;
+
+public:
+    VAO();
+
+    void bind();
+    static void unbind();
+    void deleteArray();
+    void linkVBO(const VBO& vbo, uint8 slot, uint8 n_components, GLenum type, uint8 stride, uint8 offset);
+};
---
+++ src/vao.cpp
@@ -0,0 +1,34 @@
+#include "vao.hpp"
+
+VAO::VAO()
+{
+    glGenVertexArrays(1, &id_);
+}
+
+void VAO::bind()
+{
+    glBindVertexArray(id_);
+}
+
+void VAO::unbind()
+{
+    glBindVertexArray(0);
+}
+
+void VAO::deleteArray()
+{
+    glDeleteVertexArrays(1, &id_);
+}
+
+void VAO::linkVBO(const VBO &vbo, uint8 slot, uint8 n_components, GLenum type, uint8 stride, uint8 offset)
+{
+    bind();
+    vbo.bind();
+
+    glVertexAttribPointer(slot, n_components, type, GL_FALSE, stride, reinterpret_cast<void *>(offset));
+
+    glEnableVertexAttribArray(slot);
+
+    vbo.unbind();
+    unbind();
+}
