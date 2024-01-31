f8ec92fbfe843a3a66c23f7f47e7e28644b1d587
Milad Zarei <miliaxe0@gmail.com>
2024-02-01 01:03:25 +0330

    Format file according to clangformat

--- src/vao.cpp
+++ src/vao.cpp
@@ -1,35 +1,24 @@
 #include "vao.hpp"
 #include "vbo.hpp"
 
-VAO::VAO()
-{
-    glGenVertexArrays(1, &id_);
-}
+VAO::VAO() { glGenVertexArrays(1, &id_); }
 
-void VAO::bind()
-{
-    glBindVertexArray(id_);
-}
+void VAO::bind() { glBindVertexArray(id_); }
 
-void VAO::unbind()
-{
-    glBindVertexArray(0);
-}
+void VAO::unbind() { glBindVertexArray(0); }
 
-void VAO::deleteArray()
-{
-    glDeleteVertexArrays(1, &id_);
-}
+void VAO::deleteArray() { glDeleteVertexArrays(1, &id_); }
 
-void VAO::linkVBO(const VBO &vbo, uint8 slot, uint8 n_components, GLenum type, uint8 stride, uint8 offset)
-{
-    bind();
-    vbo.bind();
+void VAO::linkVBO(VBO &vbo, uint8 slot, uint8 n_components, GLenum type,
+                  uint8 stride, uint8 offset) {
+  bind();
+  vbo.bind();
 
-    glVertexAttribPointer(slot, n_components, type, GL_FALSE, stride, reinterpret_cast<void *>(offset));
+  glVertexAttribPointer(slot, n_components, type, GL_FALSE, stride,
+                        reinterpret_cast<void *>(offset));
 
-    glEnableVertexAttribArray(slot);
+  glEnableVertexAttribArray(slot);
 
-    vbo.unbind();
-    unbind();
+  vbo.unbind();
+  unbind();
 }
