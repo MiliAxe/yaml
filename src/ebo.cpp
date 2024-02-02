37bf34bcad0c26c3a337e40b506747b68cda8fdb
Sepehr <106114097+Sephixum@users.noreply.github.com>
2024-02-02 21:27:30 +0330

    ebo data linkage fixed

--- src/ebo.cpp
+++ src/ebo.cpp
@@ -11,6 +11,8 @@ void EBO::unbind() noexcept { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
 void EBO::deleteBuffer() const noexcept { glDeleteBuffers(1, &id_); }
 
 void EBO::setData(const std::vector<GLuint> &indices) const noexcept {
+  bind();
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint),
                indices.data(), GL_STATIC_DRAW);
+  unbind();
 }
