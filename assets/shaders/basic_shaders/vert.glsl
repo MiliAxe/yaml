6d4f10f5dce9bd6866b7505a8d1487bed1d4c7af
MiliAxe <miliaxe0@gmail.com>
2024-02-04 19:46:25 +0330

    Refactor code to initialize cube vertices, buffers, textures, and transforms and add coordinate system transforms

--- assets/shaders/basic_shaders/vert.glsl
+++ assets/shaders/basic_shaders/vert.glsl
@@ -1,18 +1,3 @@
-// #version 460 core
-
-// layout(location = 0) in vec3 a_pos;
-// layout(location = 1) in vec3 a_color;
-// layout(location = 2) in vec2 a_uv;
-
-// out vec3 v_f_color;
-// out vec2 v_f_uv;
-
-// void main() {
-//     v_f_color = a_color;
-//     v_f_uv = a_uv;
-//     gl_Position = vec4(a_pos, 1.0);
-// }
-
 #version 460 core
 
 layout(location = 0) in vec3 a_pos;
@@ -22,10 +7,12 @@ layout(location = 2) in vec2 a_uv;
 out vec3 v_f_color;
 out vec2 v_f_uv;
 
-uniform float time; // Add a time uniform
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
     v_f_color = a_color;
-    v_f_uv = a_uv + vec2(sin(a_pos.y + time), 0.0); // Add a sine wave to the UV coordinates
-    gl_Position = vec4(a_pos, 1.0);
+    v_f_uv = a_uv;
+    gl_Position = projection * view * model * vec4(a_pos, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,162 @@
 #include "ebo.hpp"
+#include "globals.hpp"
 #include "shader_program.hpp"
+#include "texture2d.hpp"
 #include "utl.hpp"
 #include "vao.hpp"
 #include "vbo.hpp"
 #include "vertex.hpp"
-#include "texture2d.hpp"
 
 #include <GLFW/glfw3.h>
 #include <cstddef>
-#include <vector>
+#include <glm/ext.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <stb_image.h>
-#include <iostream>
-#include <random>
+#include <vector>
 
-std::vector<Vertex> vertices;
+float vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-std::vector<unsigned int> indices = {
-    0, 1, 3, // first triangle
-    1, 2, 3  // second triangle
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+void initializeCubeVertices(std::vector<Vertex> &VBO_vertices) {
+  VBO_vertices.reserve(36);
+  for (uint8 i = 0; i < 36; ++i) {
+    Vertex tmp_vertex;
+    tmp_vertex.position = {vertices[i * 5 + 0], vertices[i * 5 + 1],
+                           vertices[i * 5 + 2]};
+    tmp_vertex.uv = {vertices[i * 5 + 3], vertices[i * 5 + 4]};
+    VBO_vertices.push_back(tmp_vertex);
+  }
+}
+
+void initializeCubeBuffers(VAO &vao, VBO &vbo) {
+  std::vector<Vertex> VBO_vertices;
+  initializeCubeVertices(VBO_vertices);
+  vbo.setData(VBO_vertices);
+  vao.linkVBO(vbo, 0, 3, GL_FLOAT, sizeof(Vertex), 0);
+  vao.linkVBO(vbo, 1, 3, GL_FLOAT, sizeof(Vertex), offsetof(Vertex, color));
+  vao.linkVBO(vbo, 2, 2, GL_FLOAT, sizeof(Vertex), offsetof(Vertex, uv));
+}
+
+void initializeCubeTextures(Texture2D &text0, Texture2D &text1) {
+  text0.loadFromFile("assets/textures/brandon.png");
+  text1.loadFromFile("assets/textures/brandon2.png");
+}
+
+void initializeOpenGLParams() {
+  glEnable(GL_BLEND);
+  glEnable(GL_DEPTH_TEST);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void initializeTransforms(glm::mat4 &model, glm::mat4 &view, glm::mat4 &projection) {
+  model = glm::mat4(1.0f);
+  view = glm::mat4(1.0f);
+  projection = glm::mat4(1.0f);
+  projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH/(float)WINDOW_HEIGHT, 0.1f, 100.0f);
+}
+
+void updateModelTransform(glm::mat4& model) {
+  model = glm::mat4(1.0f);
+  model = glm::translate(model, glm::vec3(0.0f, 0.0f, glm::sin(glfwGetTime()) - 3.0f));
+  model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 2.0f, 1.0f));
+}
+
+void updateShaderTransforms(ShaderProgram& shader, glm::mat4& model, glm::mat4& view, glm::mat4& projection) {
+  shader.setMat4("model", model);
+  shader.setMat4("view", view);
+  shader.setMat4("projection", projection);
+}
+
+void bindTexturesToShader(ShaderProgram& shader, Texture2D& text0, Texture2D& text1) {
+  shader.activate();
+  text0.bind(0);
+  text1.bind(1);
+  shader.setInt("tex0", text0.getCurrentSlot());
+  shader.setInt("tex1", text1.getCurrentSlot());
+  shader.deActive();
+}
+
+void render(GLFWwindow* window, ShaderProgram& shader, VAO& vao) {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  shader.activate();
+  vao.bind();
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  glfwSwapBuffers(window);
+  glfwPollEvents();
+}
+
 int main() {
-    stbi_set_flip_vertically_on_load(true);
+  stbi_set_flip_vertically_on_load(true);
   GLFWwindow *window = fn::initGLFWandGLAD();
 
   ShaderProgram basic_shader("assets/shaders/basic_shaders/vert.glsl",
                              "assets/shaders/basic_shaders/frag.glsl");
 
-  vertices.reserve(4);
-  Vertex a;
-  a.position = {0.5f, 0.5f, 0.0f};
-  a.color = {0.32f, 0.24f, 0.87f};
-  a.uv = {1.0f, 1.0f};
-  Vertex b;
-  b.position = {0.5f, -0.5f, 0.0f};
-  b.color = {0.25f, 0.57f, 0.17f};
-  b.uv = {1.0f, 0.0f};
-  Vertex c;
-  c.position = {-0.5f, -0.5f, 0.0f};
-  c.color = {0.32f, 0.13f, 0.40f};
-  c.uv = {0.0f, 0.0f};
-  Vertex d;
-  d.position = {-0.5f, 0.5f, 0.0f};
-  d.color = {0.17f, 0.54f, 0.97f};
-  d.uv = {0.0f, 1.0f};
-
-  vertices.push_back(a);
-  vertices.push_back(b);
-  vertices.push_back(c);
-  vertices.push_back(d);
-
   VAO vao;
   VBO vbo;
-  vbo.setData(vertices);
-  vao.linkVBO(vbo, 0, 3, GL_FLOAT, sizeof(Vertex), 0);
-  vao.linkVBO(vbo, 1, 3, GL_FLOAT, sizeof(Vertex), offsetof(Vertex, color));
-  vao.linkVBO(vbo, 2, 2, GL_FLOAT, sizeof(Vertex) , offsetof(Vertex, uv));
+  initializeCubeBuffers(vao, vbo);
 
-  EBO ebo;
-  ebo.setData(indices);
+  Texture2D text0, text1;
+  initializeCubeTextures(text0, text1);
 
-  Texture2D text0;
-  text0.loadFromFile("assets/textures/brandon.png");
+  bindTexturesToShader(basic_shader, text0, text1);
 
-  Texture2D text1;
-  text1.loadFromFile("assets/textures/brandon2.jpg");
-  
-  std::default_random_engine generator;
-  std::uniform_real_distribution<float> distribution(0.0, 1.0);
+  initializeOpenGLParams();
 
-  // Enable blending
-  glEnable(GL_BLEND);
-  // Set the blend function
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glm::mat4 model, view, projection;
+  initializeTransforms(model, view, projection);
 
   glClearColor(0, 0, 0, 0);
   while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
+    updateModelTransform(model);
 
-    float randomValue = distribution(generator);
-    std::cout << randomValue * 100 << std::endl;
-    basic_shader.setFloat("time", randomValue * 100);
+    updateShaderTransforms(basic_shader, model, view, projection);
 
-    basic_shader.activate();
-    text0.activateSlot(0);
-    text0.bind();
-    text1.bind(1);
-    basic_shader.setInt("tex0", text0.getCurrentSlot());
-    basic_shader.setInt("tex1", text1.getCurrentSlot());
-    vao.bind();
-    ebo.bind();
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    render(window, basic_shader, vao);
   }
 
   basic_shader.deleteProgram();
